8b5336ac779994fc66e753e8a40633ae7a309e19
jc4cr8 <jc4cr8@inf.elte.hu>
2020-03-12 22:19:29 +0100

    Buttons, basic layout added

--- src/app/components/home_page/HomePage.tsx
+++ src/app/components/home_page/HomePage.tsx
@@ -4,7 +4,25 @@ import './HomePage.scss';
 function HomePage() {
     return (
         <div className="HomePage">
-            HomePage
+            <button>-</button>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+            </p>
+            <button>Témák</button>
+            <button>Új téma létrehozása</button>
+            <p>Ajánlott témák: </p>
+            <button>Analízis 1</button>
+            <br />
+            <button>Logika</button>
+            <br />
+            <button>Analízis 100</button>
+            <br />
+            <button>...</button>
+            <br />
         </div>
     );
 }
